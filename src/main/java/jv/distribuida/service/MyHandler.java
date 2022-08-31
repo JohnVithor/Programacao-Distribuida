ba1fb79eb203c6a8171fe61bc49c3d6692b65302
JohnVithor <jv.venceslau.c@gmail.com>
2022-08-31 15:21:06 -0300

    melhorando o handler de requests

--- src/main/java/jv/distribuida/service/MyHandler.java
+++ src/main/java/jv/distribuida/service/MyHandler.java
@@ -1,5 +1,6 @@
 package jv.distribuida.service;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonSyntaxException;
@@ -8,17 +9,71 @@ import jv.distribuida.network.RequestHandler;
 
 public class MyHandler implements RequestHandler {
 
+    private final String missingAction;
+
+    public MyHandler() {
+        this.missingAction = "{\"status\":\"Failure\",\"Message\":\"The 'action' attribute was not found\"}";
+    }
+
     @Override
     public Message handle(Message message) {
         try {
             JsonObject json = JsonParser.parseString(message.getText()).getAsJsonObject();
-            String action = json.get("action").getAsString();
-            System.out.println(action);
-            message.setText("OK-"+action);
+            JsonElement actionElem = json.get("action");
+            if (actionElem == null) {
+                message.setText(missingAction);
+                return message;
+            }
+            String action = actionElem.getAsString();
+            switch (action) {
+                case "CREATE" -> message.setText(createHandler(json));
+                case "UPDATE" -> message.setText(updateHandler(json));
+                case "GET" -> message.setText(getHandler(json));
+                case "FIND" -> message.setText(findHandler(json));
+                default -> message.setText(defaultHandler(action));
+            }
             return message;
-        }catch (JsonSyntaxException | IllegalStateException e) {
-            message.setText(e.getMessage());
+        } catch (JsonSyntaxException | IllegalStateException e) {
+            message.setText(exceptionHandler(e.getMessage()));
             return message;
         }
+    }
+
+    String createHandler(JsonObject json) {
+        JsonObject response = new JsonObject();
+        response.addProperty("status", "Success");
+        return response.toString();
+    }
+
+    String updateHandler(JsonObject json) {
+        JsonObject response = new JsonObject();
+        response.addProperty("status", "Success");
+        return response.toString();
+    }
+
+    String getHandler(JsonObject json) {
+        JsonObject response = new JsonObject();
+        response.addProperty("status", "Success");
+        return response.toString();
+    }
+
+    String findHandler(JsonObject json) {
+        JsonObject response = new JsonObject();
+        response.addProperty("status", "Success");
+        return response.toString();
+    }
+
+    String defaultHandler(String action) {
+        JsonObject json = new JsonObject();
+        json.addProperty("status", "Failure");
+        json.addProperty("Message", "Action: '" + action + "' is not supported");
+        return json.toString();
+    }
+
+    String exceptionHandler(String message) {
+        JsonObject json = new JsonObject();
+        json.addProperty("status", "Failure");
+        json.addProperty("Message", message);
+        return json.toString();
     }
 }
